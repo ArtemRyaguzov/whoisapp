c83bb8bfc2af1f5356a1a5b9b9ee9335741ca4a5
Artem Ryaguzov <artemkaryaguzov@gmail.com>
2020-03-29 19:48:41 +0400

    cleanup before rebuild

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const logger = require('./middleware/logger');
 
 const app = express();
 
 // Middleware
-// Custom middleware
-app.use(logger);
-
 // View Engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
 
 // Body parser middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Path to static assets
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // End of Middleware
 
@@ -28,6 +24,7 @@ app.use('/', require('./routes/main-router'));
 
 // End of Routes
 
+const hostname = '127.0.0.1';
 const port = 3001;
 
-const server = app.listen(port, () => process.stdout.write(`Server started on port ${port}...\n`));
+app.listen(port, hostname, () => console.log(`Server started on port ${port}...`));
