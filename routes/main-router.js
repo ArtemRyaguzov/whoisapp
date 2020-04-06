a8a00e3fe4ee540dfeda4580b2636ac3d27b14d7
Artem Ryaguzov <artemkaryaguzov@gmail.com>
2020-04-06 22:13:39 +0400

    core functionality refactored

--- routes/main-router.js
+++ routes/main-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const punycode = require('punycode');
+const whois = require('whois');
+const dns = require('dns').promises;
 
 const validateUserInput = str => {
     return str.trim().toLowerCase().replace(/^https?\:\/\/(www.)?|\/$/gmi, '');
@@ -14,13 +16,100 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
 
-    const validUserInput = validateUserInput(req.body.domain);
+    const { target, operation } = req.body;
 
-    console.log(validUserInput);
+    const validUserInput = validateUserInput(target);
 
-    res.render('index');
+    const validTarget = punycode.toASCII(validUserInput);
+
+
+    let result;
+    let output;
+    let errorMessage;
+
+    if(operation === 'punycode'){
+        return res.render('index', {
+            clientIp: req.headers['x-real-ip'],
+            title: 'Преобразованный домен',
+            validTarget
+        });
+    }
+
+    whois.lookup(validTarget, (err, data) => {
+        if(err){
+            console.error(err);
+            errorMessage = 'Request failed';
+        } else {
+            output = data.split('\n');
+        }
+
+        if(operation === 'whois'){
+            return res.render('index', {
+                clientIp: req.headers['x-real-ip'],
+                title: 'Ответ предоставлен ниже',
+                errorMessage,
+                output
+            });
+        }
+
+        dns.resolve(validTarget, 'ANY')
+        .then(addresses => {
+    
+            result = addresses.map(i => JSON.stringify(i));
+
+            res.render('index', {
+                clientIp: req.headers['x-real-ip'],
+                title: 'Ответ предоставлен ниже',
+                result,
+                subTitle: operation === 'dns' ? undefined : 'Информация о доменной зоне',
+                subHeader: operation === 'dns' ? undefined : 'Преобразованный домен',
+                output: operation === 'dns' ? undefined : output,
+                validTarget: operation === 'dns' ? undefined : validTarget,
+                errorMessage: operation === 'dns' ? undefined : errorMessage
+            });
+        })
+        .catch(err => {
+            console.error(err);
+
+            if(err.code === 'ENOTFOUND'){
+                return dns.reverse(validTarget)
+                    .then(hostnames => {
+                        
+                        result = hostnames;
+
+                        res.render('index', {
+                            clientIp: req.headers['x-real-ip'],
+                            title: 'Ответ предоставлен ниже',
+                            result,
+                            subTitle: operation === 'dns' ? undefined : 'Информация о доменной зоне',
+                            subHeader: operation === 'dns' ? undefined : 'Преобразованный домен',
+                            output: operation === 'dns' ? undefined : output,
+                            validTarget: operation === 'dns' ? undefined : validTarget,
+                            errorMessage: operation === 'dns' ? undefined : errorMessage
+                        });
+                    })
+                    .catch(err => {
+                        console.error(err);
+
+                        res.render('index', {
+                            clientIp: req.headers['x-real-ip'],
+                            errorMessage: `Request Failed: ${err.code}`,
+                            output: operation === 'dns' ? undefined : output
+                        });
+                    });
+            }
+
+            res.render('index', {
+                clientIp: req.headers['x-real-ip'],
+                errorMessage: `Request Failed: ${err.code}`,
+                subHeader: operation === 'dns' ? undefined : 'Преобразованный домен',
+                output: operation === 'dns' ? undefined : output,
+                validTarget: operation === 'dns' ? undefined : validTarget
+            });
+        });
+    });
+
 });
 
 module.exports = router;
